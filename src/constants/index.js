b448a69acd79e19d978859c5b0421e79a94c3507
Humberto Navarro <a320669@uach.mx>
2023-05-04 20:41:37 -0600

    Rename skills.scss and add navigation links

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home",
+    },
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "skills",
+        title: "Skills",
+    },
+    {
+        id: "portfolio",
+        title: "Portfolio",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
