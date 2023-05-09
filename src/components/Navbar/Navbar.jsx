9d75f5ff3acb5bb0cb939e2e1426f956796be693
Humberto Navarro <a320669@uach.mx>
2023-05-08 22:23:23 -0600

    Add responsive and animation navbar elements and css styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 import './Navbar.scss';
 import { navLinks, socialMedia } from '../../constants';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { animate, motion } from 'framer-motion';
 
 const Navbar = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const menuVariants = {
+    hidden: {
+      scale: 0
+    },
+    visible: {
+      scale: 50,
+      transition: {
+        type: "tween",
+        duration: 0.1,
+      },
+    },
+  }
+  const navLinkVariants = {
+    hidden: {
+      display: "none",
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      y: -30,
+      transition: {
+        delay: 0.1,
+      },
+    },
+  }
+
   return (
     <header className='header'>
       <nav className='nav-container'>
@@ -26,6 +56,31 @@ const Navbar = () => {
               </div>
             ))}
         </div>
+
+        {/* Mobile Menu */}
+        <div className="menu">
+          <HiMenuAlt4 onClick={() => setMenuToggle(true)} />
+        </div>
+
+        <motion.div className="closeMenu"
+          variants={menuVariants}
+          initial="hidden"
+          animate={menuToggle ? "visible" : "hidden"}
+        ></motion.div>
+
+        <motion.div className="menuX"
+          variants={navLinkVariants}
+          animate={menuToggle ? "visible" : "hidden"}
+        >
+          <HiX onClick={() => setMenuToggle(false)} />
+          {navLinks.map((navLink, index) => (
+            <li key={navLink.id}>
+              <a href={`#${navLink.id}`}>
+                {navLink.title}
+              </a>
+            </li>
+          ))}
+        </motion.div>
       </nav>
     </header>
   )
