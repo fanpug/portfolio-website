9c110ee7924027049a1d1f6310a5ea6813a51bd2
Humberto Navarro <a320669@uach.mx>
2023-05-08 21:12:39 -0600

    Refactor navbar component and css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,34 @@
 import './Navbar.scss';
 import { navLinks, socialMedia } from '../../constants';
 
-const Navbar = () => (
-  <header className='header'>
-    <nav className='nav-container'>
-      <div className='logo'>
-        <h3>HN</h3>
-      </div>
+const Navbar = () => {
+  return (
+    <header className='header'>
+      <nav className='nav-container'>
+        <div className='logo'>
+          <h3>HN</h3>
+        </div>
 
-      <ul className='nav-links'>
-        {navLinks.map((navLink, index) => (
-          <li key={navLink.id}>
-            <a href={`#${navLink.id}`}>
-              {navLink.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-
-      <div className='social-icons'>
-          {socialMedia.map((icon, index) => (
-            <span key={index}>
-              {icon}
-            </span>
+        <ul className='nav-links'>
+          {navLinks.map((navLink, index) => (
+            <li key={navLink.id}>
+              <a href={`#${navLink.id}`}>
+                {navLink.title}
+              </a>
+            </li>
           ))}
-      </div>
-    </nav>
-  </header>
-)
+        </ul>
+
+        <div className='social-icons'>
+            {socialMedia.map((icon, index) => (
+              <div key={index}>
+                {icon}
+              </div>
+            ))}
+        </div>
+      </nav>
+    </header>
+  )
+}
 
 export default Navbar
