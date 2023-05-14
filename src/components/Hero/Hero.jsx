ef44d56571dfe894f752053911374183cfa8a689
Humberto Navarro <a320669@uach.mx>
2023-05-13 22:32:57 -0600

    Add hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,28 @@
 import './Hero.scss';
+import { portfolio } from '../../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='container' id='home'>
+      <div className="profile">
+        <img src={portfolio} alt="Portfolio image" />
+      </div>
+      <div className="profile-text">
+        <h3 className='name'>
+          Hi, I'm <span>Alex Navarro</span>
+        </h3>  
+        <h4 className='job'>Web Developer</h4>
+        <p className='text'>
+          Passionate <br />
+          to craft innovative <br />
+          web products.
+        </p>
+        <a href="contact">connect with me</a>
+        <div className="web">Web Developer</div>
+        <div className="ui">UI/UX Designer</div>
+        <div className="freelance">Freelancer</div>
+      </div>
+    </section>
   )
 }
 
