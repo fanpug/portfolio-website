e43e488fb55a0e15b6a6f78ca3255bd90723957b
Humberto Navarro <a320669@uach.mx>
2023-05-16 19:23:40 -0600

    Add about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,42 @@
 import './About.scss';
+import { portfolio } from '../../assets';
+import { bios } from '../../constants';
+import { motion } from 'framer-motion';
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='container' id='about'>
+      <div className='title'>
+        <span>Who Am I?</span>
+        <h1>About Me</h1>
+
+      </div>
+      <div className="about-container">
+        <div className="about-left">
+          <img src={portfolio} alt="About image" />
+        </div>
+
+        <div className="about-right">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil sit nam itaque debitis fugit accusantium officia praesentium iusto reprehenderit soluta ad, delectus dolor voluptate. Id harum voluptatem consequuntur commodi sunt.
+          </p>
+          {bios.map((bio) => {
+            return (
+              <div className='bio' key={bio.id}>
+                <span className='bioKey'>{bio.icon}{bio.key}</span>
+                <span className='bioValue'>{bio.value}</span>
+              </div>
+            );
+          })}
+          <motion.a href="#" download=""
+            whileHover={{scale: 1.1}}
+            transition={{duration: 0.3}}
+          >
+            Download Resume
+          </motion.a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,30 +1,41 @@
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { FaHtml5, FaCss3, FaNodeJs, FaReact, FaSass, FaFigma, FaPhoneAlt, FaUser, FaMapMarkerAlt, FaPaperPlane, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const navLinks = [
-    {
-        id: "home",
-        title: "Home",
-    },
-    {
-        id: "about",
-        title: "About",
-    },
-    {
-        id: "skills",
-        title: "Skills",
-    },
-    {
-        id: "portfolio",
-        title: "Portfolio",
-    },
-    {
-        id: "contact",
-        title: "Contact",
-    },
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "skills",
+    title: "Skills",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
 ];
 
-export const socialMedia = [
-    <FaLinkedin />,
-    <FaTwitter />,
-    <FaGithub />,
+export const socialMedia = [<FaLinkedin />, <FaTwitter />, <FaGithub />];
+
+export const bios = [
+  {
+    id: 1,
+    icon: <FaUser />,
+    key: "Name",
+    value: "Alex",
+  },
+  {
+    id: 2,
+    icon: <FaPaperPlane />,
+    key: "Email",
+    value: "navarro.andujo444@yahoo.com",
+  },
 ];
