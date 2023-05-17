dbd2505248167d33ba584792c71e8e023748fc0a
Humberto Navarro <a320669@uach.mx>
2023-05-16 22:20:26 -0600

    Add skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,69 @@
-import './Skills.scss';
+import { useState } from "react";
+import { icons, experiences, flavorNumbers } from "../../constants";
+import "./Skills.scss";
 
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+  const [activeButton, setActiveButton] = useState(1);
 
-export default Skills
+  return (
+    <section className="container" id="skills">
+      <div className="title">
+        <span>What I Expert</span>
+        <h1>Skills And Experience</h1>
+      </div>
+
+      <div className="select">
+        <button
+          onClick={() => setActiveButton(1)}
+          className={activeButton === 1 ? "active" : ""}
+        >
+          Skills
+        </button>
+
+        <button
+          onClick={() => setActiveButton(2)}
+          className={activeButton === 2 ? "active" : ""}
+        >
+          Experience
+        </button>
+      </div>
+
+      <div className='skills'>
+        {activeButton === 1 && icons.map((icon, idx) => 
+          <div key={idx} className='icon'>
+            {icon}
+          </div>
+        )}
+      </div>
+
+      <div className='experiences'>
+        {activeButton === 2 && experiences.map((experience) => 
+          <div key={experience.id} className='experience'>
+            <span>
+              {experience.year}
+            </span>
+            <div className='position'>
+              <h3>{experience.position}</h3>
+              <p>{experience.company}</p>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className='flavor-numbers'>
+        {flavorNumbers.map((number) => 
+          <div key={number.id} className='flavor-number'>
+            <div className='number'>
+              {number.number}
+            </div>
+            <div className="flavor-name">
+              {number.itemName}
+            </div>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,5 +1,19 @@
-import { FaHtml5, FaCss3, FaNodeJs, FaReact, FaSass, FaFigma, FaPhoneAlt, FaUser, FaMapMarkerAlt, FaPaperPlane, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
-
+import {
+  FaHtml5,
+  FaCss3,
+  FaNodeJs,
+  FaReact,
+  FaSass,
+  FaFigma,
+  FaUser,
+  FaPaperPlane,
+  FaTwitter,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+import { DiJavascript } from "react-icons/di";
+import { SiTypescript } from "react-icons/si";
+import { BsMedium } from "react-icons/bs";
 export const navLinks = [
   {
     id: "home",
@@ -37,5 +51,60 @@ export const bios = [
     icon: <FaPaperPlane />,
     key: "Email",
     value: "navarro.andujo444@yahoo.com",
+  },
+];
+
+export const icons = [
+  <FaHtml5 />,
+  <FaCss3 />,
+  <DiJavascript />,
+  <SiTypescript />,
+  <FaReact />,
+  <FaNodeJs />,
+  <FaSass />,
+  <FaFigma />,
+];
+
+export const experiences = [
+  {
+    id: 1,
+    year: "2022",
+    position: "Senior Web Developer",
+    company: "shop.com.mm",
+  },
+  {
+    di: 2,
+    year: "2021",
+    position: "Junior Web Developer",
+    company: "shop.com.mm",
+  },
+  {
+    id: 3,
+    year: "2019",
+    position: "Intern Web Dveloper",
+    company: "shop.com.mm",
+  },
+];
+
+export const flavorNumbers = [
+  {
+    id: 1,
+    number: "6+",
+    itemName: "Years Of Experience",
+  },
+  {
+    id: 2,
+    number: "150+",
+    itemName: "Satisfied Customers",
+  },
+  {
+    id: 3,
+    number: "669+",
+    itemName: "Designed Items",
+  },
+  {
+    id: 4,
+    number: "117+",
+    itemName: "Clients Served",
   },
 ];
