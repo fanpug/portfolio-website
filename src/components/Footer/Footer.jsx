299d06af046e0c432ae3c75e38ff07d855c4e4d2
Humberto Navarro <Humberto Navarro>
2023-05-30 10:57:05 -0600

    Add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,25 @@
 import './Footer.scss';
+import { socialMedia } from '../../constants';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className="copyright">
+        <p>Copyright &copy; 2023 All rights reserved | Made by <span>Humberto Navarro</span></p>
+      </div>
+
+      <div className="follow-me">
+        <h4>Follow Me</h4>
+        <div className="stick"></div>
+        <div className="footer-social-icons">
+          {socialMedia.map((socialIcon, idx) => 
+            <div key={idx}>
+              {socialIcon}
+            </div>
+          )}
+        </div>
+      </div>
+    </footer>
   )
 }
 
